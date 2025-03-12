9060c20f620d9567aa80b4e1af7d2f353c1d91cd
Quinten Althues <quinten@althues.nl>
2025-03-12 22:12:28 +0100

    Increase limit

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Limit request body size to 50mb
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // Enable CORS
 app.use(cors({ origin: '*' })); // Allow all origins
 
